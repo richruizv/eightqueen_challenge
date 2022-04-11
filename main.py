bc28d2e68d0870f72bd76de0fdb8df0fbb950d19
Ricardo Ruíz <rich.ruiz.v@gmail.com>
2022-04-10 20:17:02 -0500

    catching value errors from main.py

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
+from cmath import e
+from pprint import pprint
+import time
+
 """The n queens puzzle."""
 class NQueens:
     """Generate all valid solutions for the n queens puzzle"""
-    def __init__(self, size):
+    def __init__(self, size,print_options):
         # Store the puzzle (problem) size and the number of valid solutions
         self.size = size
+        self.print_options = print_options
         self.solutions = 0
+        self.possible_solutions = []
         self.solve()
 
     def solve(self):
         """Solve the n queens puzzle and print the number of solutions"""
+
+        start_time = time.time()        
+
         positions = [-1] * self.size
         self.put_queen(positions, 0)
-        print("Found", self.solutions, "solutions.")
+
+        self.total_time = round((time.time() - start_time),2)
+        print(f"\nFound  {self.solutions} solutions. Time taken: {self.total_time}")
 
     def put_queen(self, positions, target_row):
         """
@@ -21,8 +32,16 @@ class NQueens:
         """
         # Base (stop) case - all N rows are occupied
         if target_row == self.size:
-            self.show_full_board(positions)
-            # self.show_short_board(positions)
+            
+            self.possible_solutions.append([i for i in positions]) #collecting all the solutions on this array, and with list comprehension because we want to save the values of the list
+
+            if self.print_options == 1:
+                self.show_short_board(positions)
+            elif self.print_options == 2:
+                self.show_full_board(positions)
+            elif self.print_options == 3:
+                print("Saving into database")
+            
             self.solutions += 1
         else:
             # For all N columns positions try to place a queen
@@ -55,8 +74,7 @@ class NQueens:
                 else:
                     line += ". "
             print(line)
-        print("\n")
-        print("positions", positions)
+        print("")
 
     def show_short_board(self, positions):
         """
@@ -70,7 +88,25 @@ class NQueens:
 
 def main():
     """Initialize and solve the n queens puzzle"""
-    NQueens(16)
+    try:
+        board_size = int(input("please, put the board size: "))
+        print("""
+Select to way you want to show the results
+    1: Simple board (positions of each array)
+    2: Full board ( an ASCII representation of the chess board)
+    3: Database (Saving into a Postgress database)
+    0: only the final amount of solutions
+        """)
+
+        print_options = int(input("please, pick one of the print options: "))
+
+        if print_options not in [0,1,2,3]:
+            return print("You should pick one of the given option")
+        else:
+            NQueens(board_size,print_options)
+    except ValueError as e:
+        print("You should put integer values")
+
 
 if __name__ == "__main__":
     # execute only if run as a script
